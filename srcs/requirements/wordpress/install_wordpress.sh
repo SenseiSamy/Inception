423a7c08c9e53cdacf5bd6d9527339bc007e6f5d
SenseiSamy <samy.naji2003@gmail.com>
2024-10-22 23:11:16 +0200

    abcde

--- srcs/requirements/wordpress/install_wordpress.sh
+++ srcs/requirements/wordpress/install_wordpress.sh
@@ -9,10 +9,11 @@ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp-cli.phar
 mkdir -p /run/php /var/www/html
+cd /var/www/html
 echo "downloading wordpress"
 wp-cli.phar core download
 echo "creating wordpress config"
 wp-cli.phar config create --dbname=$MARIADB_DBNAME --dbuser=$MARIADB_USER --dbpass=$MARIADB_PASSWORD --dbhost=mariadb
 echo "installing wordpress config"
 wp-cli.phar core install --url=localhost --title=Inception --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL
-exec php-fpm82 -F
+exec php-fpm82 -F -R
