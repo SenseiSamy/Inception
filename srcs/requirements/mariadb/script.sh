15e60c08179bd386b25184c5623515ae6c776430
SenseiSamy <samy.naji2003@gmail.com>
2024-10-22 19:07:53 +0200

    changed mariadb from debian to alpine

--- srcs/requirements/mariadb/script.sh
+++ srcs/requirements/mariadb/script.sh
@@ -3,14 +3,13 @@ mkdir -p /run/mysqld/
 chown -R mysql:mysql /run/mysqld
 chown -R mysql:mysql /var/lib/mysql
 echo "Launching install script"
-# mariadb-install-db
-# echo "Starting mariadb service"
-# service mariadb start
+mariadb-install-db --user mysql
 echo "Creating wordpress db and admin user"
 mariadbd --bootstrap --user mysql << EOF
 FLUSH PRIVILEGES;
 CREATE DATABASE $MARIADB_DBNAME;
-GRANT ALL PRIVILEGES ON $MARIADB_DBNAME.* TO "$MARIADB_USER"@"%" IDENTIFIED BY "$MARIADB_PASSWORD";
+CREATE USER "$MARIADB_USER"@'%' IDENTIFIED BY "$MARIADB_PASSWORD";
+GRANT ALL PRIVILEGES ON *.* TO "$MARIADB_USER"@'%' IDENTIFIED BY "$MARIADB_PASSWORD";
 FLUSH PRIVILEGES;
 exit
 EOF
