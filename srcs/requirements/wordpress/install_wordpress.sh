775c50be48356f068db4f67b53c7db5d730c8e46
SenseiSamy <samy.naji2003@gmail.com>
2024-10-24 17:10:33 +0200

    WORKING

--- srcs/requirements/wordpress/install_wordpress.sh
+++ srcs/requirements/wordpress/install_wordpress.sh
@@ -4,7 +4,7 @@
 # wget https://wordpress.org/latest.tar.gz
 # tar -xzvf latest.tar.gz
 # rm latest.tar.gz
-mv /usr/bin/php82 /usr/bin/php
+cp /usr/bin/php82 /usr/bin/php
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp-cli.phar
@@ -15,7 +15,7 @@ wp-cli.phar core download
 echo "creating wordpress config"
 wp-cli.phar config create --dbname=$MARIADB_DBNAME --dbuser=$MARIADB_USER --dbpass=$MARIADB_PASSWORD --dbhost=mariadb
 echo "installing wordpress config"
-wp-cli.phar core install --url=wordpress --title=Inception --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL
+wp-cli.phar core install --url=localhost:6969 --title=Inception --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --allow-root
 wp-cli.phar user create $WP_USER $WP_EMAIL --role=subscriber --user_pass=$WP_PASSWORD --allow-root
 wp-cli.phar theme install twentyfourteen --activate --allow-root
 wp-cli.phar post delete $(wp-cli.phar post list --format=ids --allow-root) --allow-root
